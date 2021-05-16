e0861bbae46935172191f2b6f10c9b536b76fbb3
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-17 03:45:10 +0500

    Update Team.php

--- app/Database/Seeds/Team.php
+++ app/Database/Seeds/Team.php
@@ -27,6 +27,62 @@ class Team extends Seeder
                 'id' =>'3'
             ];
             $this->db->table('team')->insert($data);
+		
+		$data = [
+
+                'name' => 'Команда 4',
+                'id' =>'4'
+            ];
+            $this->db->table('team')->insert($data);
+
+            $data = [
+
+                'name' => 'Команда 5',
+                'id' =>'5'
+            ];
+            $this->db->table('team')->insert($data);
+
+            $data = [
+
+                'name' => 'Команда 6',
+                'id' =>'6'
+            ];
+            $this->db->table('team')->insert($data);
+		
+		$data = [
+
+                'name' => 'Команда 7',
+                'id' =>'7'
+            ];
+            $this->db->table('team')->insert($data);
+
+            $data = [
+
+                'name' => 'Команда 7',
+                'id' =>'7'
+            ];
+            $this->db->table('team')->insert($data);
+
+            $data = [
+
+                'name' => 'Команда 8',
+                'id' =>'8'
+            ];
+            $this->db->table('team')->insert($data);
+		
+		 $data = [
+
+                'name' => 'Команда 9',
+                'id' =>'9'
+            ];
+            $this->db->table('team')->insert($data);
+
+            $data = [
+
+                'name' => 'Команда 10',
+                'id' =>'10'
+            ];
+            $this->db->table('team')->insert($data);
         }
 	}
 }
