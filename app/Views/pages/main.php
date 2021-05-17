2391dec521d13fce0f172b62edc7169a5a9d4f0d
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-17 18:40:36 +0500

    Update main.php

--- app/Views/pages/main.php
+++ app/Views/pages/main.php
@@ -1,7 +1,7 @@
 <?= $this->extend('templates/layout') ?>
 <?= $this->section('content') ?>
     <div class="jumbotron text-center">
-        <img class="mb-2" src="https://w7.pngwing.com/pngs/205/64/png-transparent-football-manager-handheld-football-manager-mobile-2018-paris-saint-germain-f-c-game-logo-others-game-logo-paris-saintgermain-fc.png" alt="" width="90" height="90">Football Online</h1>
+        <img class="mb-2" src="https://w7.pngwing.com/pngs/205/64/png-transparent-football-manager-handheld-football-manager-mobile-2018-paris-saint-germain-f-c-game-logo-others-game-logo-paris-saintgermain-fc.png" alt="" width="90" height="90"><h1 class="display-4">Football Online</h1>
         <p class="lead">Приложения футбольных команд.</p>
         <a class="btn btn-primary btn-lg" href="auth/login" role="button">Войти</a>
     </div>
