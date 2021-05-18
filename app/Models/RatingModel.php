fed43d7bb1f97cab44268721689e40df9915f772
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-18 11:25:34 +0500

    Update RatingModel.php

--- app/Models/RatingModel.php
+++ app/Models/RatingModel.php
@@ -2,15 +2,14 @@
 use CodeIgniter\Model;
 class RatingModel extends Model
 {
-    protected $table = 'player'; //таблица, связанная с моделью
-    //Перечень задействованных в модели полей таблицы
-    protected $allowedFields = ['id_team', 'FIO', 'Amplua','picture_url'];
-    public function getRating($id = null)
-    {
-        if (!isset($id)) {
-            return $this->findAll();
-        }
-        $builder = $this->select('*')->join('team','player.id_team = team.id');
-        return $builder->where(['player.id' => $id])->first();
-    }
+protected $table = 'player'; //таблица, связанная с моделью
+//Перечень задействованных в модели полей таблицы
+protected $allowedFields = ['id_team', 'FIO', 'Amplua','picture_url'];
+public function getRating($id = null)
+{
+if (!isset($id)) {
+return $this->findAll();
+}
+return $this->where(['id' => $id])->first();
+}
 }
