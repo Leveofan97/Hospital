4f3d7a9d2368821f083eda98a2e6ce86eaa460da
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-18 12:05:26 +0500

    Update view.php

--- app/Views/player/view.php
+++ app/Views/player/view.php
@@ -6,7 +6,13 @@
             <div class="card mb-3" style="max-width: 640px;">
                 <div class="row">
                     <div class="col-md-4 d-flex align-items-center">
-                        <img height="200" src="<?= esc($player['picture_url']); ?>" class="card-img" alt="<?= esc($player['FIO']); ?>">
+                        <img style="
+  display: block;
+  max-width:180px;
+  max-height:200px;
+  width: auto;
+  height: auto;
+  padding-left: 10%;" src="<?= esc($player['picture_url']); ?>" class="card-img" alt="<?= esc($player['FIO']); ?>">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
