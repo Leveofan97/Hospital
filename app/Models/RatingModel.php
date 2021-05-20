e95501195f228c637f4b5b023faca5cb5415524e
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-20 11:19:30 +0500

    Update RatingModel.php

--- app/Models/RatingModel.php
+++ app/Models/RatingModel.php
@@ -4,7 +4,7 @@ class RatingModel extends Model
 {
     protected $table = 'player'; //таблица, связанная с моделью
     //Перечень задействованных в модели полей таблицы
-    protected $allowedFields = ['id_team', 'FIO', 'Amplua','picture_url', 'userid'];
+    protected $allowedFields = ['id_team', 'FIO', 'Amplua','picture_url', 'userid', 'name'];
     public function getRating($id = null)
     {
         if (!isset($id)) {
