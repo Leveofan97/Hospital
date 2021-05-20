f02611266435426655cd8d2543f40c4e38c1287f
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-20 11:57:55 +0500

    Update view_all_with_users.php

--- app/Views/player/view_all_with_users.php
+++ app/Views/player/view_all_with_users.php
@@ -38,7 +38,7 @@
                         </td>
                         <td><?= esc($item['FIO']); ?></td>
                         <td><?= esc($item['email']); ?></td>
-                        <td>  <?php if($item['id_team'] == 1) ?> Aboba</td>
+                        <td>  <?php if($item['id_team'] == 1) echo("Текст")?></td>
                         <td><?= esc($item['Amplua']); ?></td>
                         <td>
                             <a href="<?= base_url()?>/player/view/<?= esc($item['id']); ?>" class="btn btn-primary btn-sm">Просмотреть</a>
