6265ac8c8358da48ccbe1a982a39516e682bf6ae
Андрей Корепанов <56936172+Leveofan97@users.noreply.github.com>
2021-05-20 11:22:44 +0500

    Update RatingModel.php

--- app/Models/RatingModel.php
+++ app/Models/RatingModel.php
@@ -16,7 +16,7 @@ class RatingModel extends Model
     public function getRatingWithUser($id = null, $search = '')
     {
         if($search != null){
-            $builder = $this->select('player.id, id_team, FIO, Amplua ,player.picture_url,email')
+            $builder = $this->select('player.id, id_team, FIO, Amplua ,player.picture_url,email,name')
                 ->join('users','player.userid = users.id')
                 ->join('team','player.id_team = team.id')
                 ->where('Amplua',$search)
